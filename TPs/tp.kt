a50d8ccf25c7df430d33f0142bbc3ebd836575e6
GHEZAL <souhaib.dz39@gmail.com>
2025-02-13 10:47:45 +0100

    tp1 up

---
+++ TPs/tp.kt
@@ -0,0 +1,132 @@
+package com.example.imatah
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.imatah.ui.theme.ImatahTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ImatahTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    FirstUI(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstUI(modifier: Modifier = Modifier) {
+    var textValue by remember { mutableStateOf("") }
+    val allItems = remember { mutableStateListOf<String>() }
+    var searchQuery by remember { mutableStateOf("") }
+
+    val displayedItems = if (searchQuery.isEmpty()) {
+        allItems
+    } else {
+        allItems.filter { it.contains(searchQuery, ignoreCase = true) }
+    }
+
+    Column(
+        modifier = modifier
+            .padding(25.dp)
+            .fillMaxSize()
+    ) {
+        SearchInputBar(
+            textValue = textValue,
+            onTextValueChange = { newValue ->
+                textValue = newValue
+            },
+            onAddItem = {
+                if (textValue.isNotBlank()) {
+                    allItems.add(textValue)
+                    textValue = ""
+                }
+            },
+            onSearch = {
+                searchQuery = textValue
+            }
+        )
+
+        CardsList(displayedItems = displayedItems)
+    }
+}
+
+@Composable
+fun SearchInputBar(
+    textValue: String,
+    onTextValueChange: (String) -> Unit,
+    onAddItem: () -> Unit,
+    onSearch: () -> Unit
+) {
+    Column {
+        TextField(
+            value = textValue,
+            onValueChange = onTextValueChange,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text("... إضافة نص " , fontSize = 15.sp, fontWeight = FontWeight.Bold) }
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(onClick = onAddItem, modifier = Modifier.height(60.dp).width(100.dp)) {
+                Text("إضافة" , fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            }
+
+            Button(onClick = onSearch ,modifier = Modifier.height(60.dp).width(100.dp)) {
+                Text("بحث" , fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            }
+        }
+    }
+}
+
+@Composable
+fun CardsList(displayedItems: List<String>) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(displayedItems) { item ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 7.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            ) {
+                Text(text = item, modifier = Modifier.padding(16.dp))
+            }
+        }
+    }
+}
