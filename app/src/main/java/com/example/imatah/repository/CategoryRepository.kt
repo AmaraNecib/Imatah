2ac1e7b8a9369b76733b810771fcb9325dd08aa6
abdenour39 <abdennourbediafe@gmail.com>
2025-02-26 17:23:15 +0100

    git tp002

--- app/src/main/java/com/example/imatah/repository/CategoryRepository.kt
+++ app/src/main/java/com/example/imatah/repository/CategoryRepository.kt
@@ -9,19 +9,19 @@ object CategoryRepository {
                 id = 1,
                 name = "General",
                 description = "General reports",
-                icon = "https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                imageUrl = "https://images.pexels.com/photos/30704109/pexels-photo-30704109/free-photo-of-outdoor-market-at-vienna-city-hall-in-autumn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
             ),
             Category(
                 id = 2,
                 name = "Urgent",
                 description = "Urgent reports",
-                icon = "https://cdn-icons-png.flaticon.com/512/149/149119.png"
+                imageUrl = "https://images.pexels.com/photos/30394204/pexels-photo-30394204/free-photo-of-vibrant-historic-architecture-in-mexico-city.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
             ),
             Category(
                 id = 3,
                 name = "Maintenance",
                 description = "Maintenance reports",
-                icon = "https://cdn-icons-png.flaticon.com/512/149/149120.png"
+                imageUrl = "https://images.pexels.com/photos/16870065/pexels-photo-16870065/free-photo-of-a-pink-building-with-a-large-pink-banner-on-it.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
             )
         )
     }
